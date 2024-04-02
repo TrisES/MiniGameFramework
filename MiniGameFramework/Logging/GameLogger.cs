3a69d09bba57552fc97e658f31c6091706d51258
TrisES <112662675+TrisES@users.noreply.github.com>
2024-04-02 11:28:15 +0200

    Dokumenteret GameLogger lidt mere.

--- MiniGameFramework/Logging/GameLogger.cs
+++ MiniGameFramework/Logging/GameLogger.cs
@@ -2,23 +2,36 @@
 
 namespace MiniGameFramework.Logging
 {
+    /// <summary>
+    /// Provides logging functionality for the game.
+    /// </summary>
     public static class GameLogger
     {
+        // Using built-in class, TraceSource, for logging
         private static readonly TraceSource traceSource = new TraceSource("GameTraceSource");
 
+        /// <summary>
+        /// Static constructor for the GameLogger class.
+        /// </summary>
         static GameLogger()
         {
-            // Example setup: Console trace listener
-            var consoleListener = new ConsoleTraceListener();
+            // Setup: Console trace listener. 
+            ConsoleTraceListener consoleListener = new ConsoleTraceListener();
             traceSource.Listeners.Add(consoleListener);
             traceSource.Switch.Level = SourceLevels.All; // Set the level of logging detail
 
             // Additional listeners can be added here (e.g., for logging to a file)
         }
 
+        /// <summary>
+        /// Logs a message with the specified event type.
+        /// </summary>
+        /// <param name="message">The message to be logged</param>
+        /// <param name="eventType">The type of event. If none specified, defaults to "TraceEventType.Information"</param>
         public static void Log(string message, TraceEventType eventType = TraceEventType.Information)
         {
             traceSource.TraceEvent(eventType, 0, message);
         }
     }
-}
+}   
+
