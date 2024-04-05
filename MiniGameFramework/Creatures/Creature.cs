cdaaf47f28a8c2a41dd7c8a9673ba8e0e115d715
TrisES <112662675+TrisES@users.noreply.github.com>
2024-04-05 23:34:01 +0200

    Tilføjet reflection i Creature klassen, så fremtidige subklasser ikke behøver at implementere ArmorDefenseSum og WeaponsAttackSum. Tror dog at de med fordel kan overrides i subklasser for bedre performance.

--- MiniGameFramework/Creatures/Creature.cs
+++ MiniGameFramework/Creatures/Creature.cs
@@ -1,7 +1,10 @@
 ﻿using MiniGameFramework.Creatures.CombatStrategy;
 using MiniGameFramework.Items;
+using MiniGameFramework.Items.Armor;
+using MiniGameFramework.Items.Weapons.Interfaces;
 using MiniGameFramework.Logging;
 using MiniGameFramework.WorldClasses;
+using System.Reflection;
 
 namespace MiniGameFramework.Creatures
 {
@@ -11,8 +14,64 @@ namespace MiniGameFramework.Creatures
         public int BaseDefense { get; set; }
         public int BaseAttack { get; set; }
 
-        public abstract int ArmorDefenseSum { get; }
-        public abstract int WeaponsAttackSum { get; }
+        public int ArmorDefenseSum 
+        { 
+            get 
+            {
+                int sum = 0;
+
+                // Get all properties of the creature
+                PropertyInfo[] properties = GetType().GetProperties();
+
+                // Iterate through all properties
+                foreach (PropertyInfo property in properties)
+                {
+                    // Check if the property type implements the IArmor interface
+                    if (typeof(IArmor).IsAssignableFrom(property.PropertyType))
+                    {
+                        // Get the value of the property
+                        object? armor = property.GetValue(this);
+
+                        // If the armor is not null, add its defense value to the sum
+                        if (armor != null)
+                        {
+                            sum += ((IArmor)armor).Defense;
+                        }
+                    }
+                }
+
+                return sum;
+            }
+        }
+        public int WeaponsAttackSum 
+        { 
+            get
+            {
+                int sum = 0;
+
+                // Get all properties of the creature
+                PropertyInfo[] properties = GetType().GetProperties();
+
+                // Iterate through all properties
+                foreach (PropertyInfo property in properties)
+                {
+                    // Check if the property type implements the IWeapon interface
+                    if (typeof(IWeapon).IsAssignableFrom(property.PropertyType))
+                    {
+                        // Get the value of the property
+                        object? weapon = property.GetValue(this);
+
+                        // If the weapon is not null, add its damage value to the sum
+                        if (weapon != null)
+                        {
+                            sum += ((IWeapon)weapon).Damage;
+                        }
+                    }
+                }
+
+                return sum;
+            }
+        }
 
         public IInventory Inventory { get; set; }
         public ICombatStrategy CombatStrategy { get; set; }
