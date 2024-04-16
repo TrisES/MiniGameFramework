500fd882a1875cbbc29f7d0b669dd0218d11d6dc
TrisES <112662675+TrisES@users.noreply.github.com>
2024-04-16 13:36:59 +0200

    Kommentarer/dokumentation af repo base klasserne.

--- MiniGameFramework/Repository/Base/GuidRepoBaseDictionary.cs
+++ MiniGameFramework/Repository/Base/GuidRepoBaseDictionary.cs
@@ -3,17 +3,35 @@ using MiniGameFramework.MarkerInterfaces;
 
 namespace MiniGameFramework.Repository.Base
 {
+    /// <summary>
+    /// A base repository class for managing entities with GUIDs using a dictionary for storage.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity. Must implement IHasGuid.</typeparam>
     public class GuidRepoBaseDictionary<T> : IRepositoryGUID<T> where T : class, IHasGuid
     {
+        /// <summary>
+        /// The dictionary of entities.
+        /// </summary>
         protected readonly Dictionary<Guid, T> _dictionary;
 
+        /// <summary>
+        /// Gets the count of entities in the repository.
+        /// </summary>
         public int Count => _dictionary.Count;
 
-        public GuidRepoBaseDictionary() 
+        /// <summary>
+        /// Initializes a new instance of the GuidRepoBaseDictionary class.
+        /// </summary>
+        public GuidRepoBaseDictionary()
         {
             _dictionary = new Dictionary<Guid, T>();
         }
 
+        /// <summary>
+        /// Adds a new entity to the repository.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <returns>The added entity.</returns>
         public T Add(T entity)
         {
             entity.Id = Guid.NewGuid();
@@ -21,16 +39,30 @@ namespace MiniGameFramework.Repository.Base
             return entity;
         }
 
+        /// <summary>
+        /// Gets an entity by its GUID.
+        /// </summary>
+        /// <param name="id">The GUID of the entity.</param>
+        /// <returns>The entity if found; otherwise, null.</returns>
         public T? GetById(Guid id)
         {
             return _dictionary.TryGetValue(id, out T? entity) ? entity : null;
         }
 
+        /// <summary>
+        /// Gets all entities in the repository.
+        /// </summary>
+        /// <returns>A list of all entities.</returns>
         public IEnumerable<T> Get()
         {
             return _dictionary.Values;
         }
 
+        /// <summary>
+        /// Deletes an entity by its GUID.
+        /// </summary>
+        /// <param name="id">The GUID of the entity.</param>
+        /// <returns>The deleted entity if found; otherwise, null.</returns>
         public T? Delete(Guid id)
         {
             if (_dictionary.TryGetValue(id, out T? entity))
@@ -41,16 +73,31 @@ namespace MiniGameFramework.Repository.Base
             return null;
         }
 
+        /// <summary>
+        /// Checks if an entity with the specified GUID exists in the repository.
+        /// </summary>
+        /// <param name="id">The GUID of the entity.</param>
+        /// <returns>True if the entity exists; otherwise, false.</returns>
         public bool Exists(Guid id)
         {
             return _dictionary.ContainsKey(id);
         }
 
+        /// <summary>
+        /// Gets a list of entities that match the specified predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate to match.</param>
+        /// <returns>A list of matching entities.</returns>
         public IEnumerable<T> Get(Func<T, bool> predicate)
         {
             return _dictionary.Values.Where(predicate);
         }
 
+        /// <summary>
+        /// Gets the first entity that matches the specified predicate.
+        /// </summary>
+        /// <param name="predicate">The predicate to match.</param>
+        /// <returns>The first matching entity if found; otherwise, null.</returns>
         public T? GetFirst(Func<T, bool> predicate)
         {
             return _dictionary.Values.FirstOrDefault(predicate);
--- MiniGameFramework/Repository/Base/GuidRepoBaseList.cs
+++ MiniGameFramework/Repository/Base/GuidRepoBaseList.cs
@@ -3,17 +3,35 @@ using MiniGameFramework.MarkerInterfaces;
 
 namespace MiniGameFramework.Repository.Base
 {
+    /// <summary>
+    /// A base repository class for managing entities with GUIDs using a list for storage.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity. Must implement IHasGuid.</typeparam>
     public class GuidRepoBaseList<T> : IRepositoryGUID<T> where T : class, IHasGuid
     {
+        /// <summary>
+        /// The list of entities.
+        /// </summary>
         protected readonly List<T> _list;
 
+        /// <summary>
+        /// Gets the count of entities in the repository.
+        /// </summary>
         public int Count => _list.Count;
 
+        /// <summary>
+        /// Initializes a new instance of the GuidRepoBaseList class.
+        /// </summary>
         public GuidRepoBaseList()
         {
             _list = new List<T>();
         }
 
+        /// <summary>
+        /// Adds a new entity to the repository.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <returns>The added entity.</returns>
         public T Add(T entity)
         {
             entity.Id = Guid.NewGuid();
@@ -21,16 +39,30 @@ namespace MiniGameFramework.Repository.Base
             return entity;
         }
 
+        /// <summary>
+        /// Gets an entity by its GUID.
+        /// </summary>
+        /// <param name="id">The GUID of the entity.</param>
+        /// <returns>The entity if found; otherwise, null.</returns>
         public T? GetById(Guid id)
         {
             return _list.FirstOrDefault(e => e.Id == id);
         }
 
+        /// <summary>
+        /// Gets all entities in the repository.
+        /// </summary>
+        /// <returns>A list of all entities.</returns>
         public IEnumerable<T> Get()
         {
             return _list;
         }
 
+        /// <summary>
+        /// Deletes an entity by its GUID.
+        /// </summary>
+        /// <param name="id">The GUID of the entity.</param>
+        /// <returns>The deleted entity if found; otherwise, null.</returns>
         public T? Delete(Guid id)
         {
             T? entityToDelete = GetById(id);
@@ -45,8 +77,8 @@ namespace MiniGameFramework.Repository.Base
         /// <summary>
         /// Checks if an entity with the specified GUID exists in the repository.
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">The GUID of the entity.</param>
+        /// <returns>True if the entity exists; otherwise, false.</returns>
         public bool Exists(Guid id)
         {
             return _list.Any(e => e.Id == id);
@@ -55,8 +87,8 @@ namespace MiniGameFramework.Repository.Base
         /// <summary>
         /// Gets a list of entities that match the specified predicate.
         /// </summary>
-        /// <param name="predicate"></param>
-        /// <returns></returns>
+        /// <param name="predicate">The predicate to match.</param>
+        /// <returns>A list of matching entities.</returns>
         public IEnumerable<T> Get(Func<T, bool> predicate)
         {
             return _list.Where(predicate);
@@ -65,8 +97,8 @@ namespace MiniGameFramework.Repository.Base
         /// <summary>
         /// Gets the first entity that matches the specified predicate.
         /// </summary>
-        /// <param name="predicate"></param>
-        /// <returns></returns>
+        /// <param name="predicate">The predicate to match.</param>
+        /// <returns>The first matching entity if found; otherwise, null.</returns>
         public T? GetFirst(Func<T, bool> predicate)
         {
             return _list.FirstOrDefault(predicate);
